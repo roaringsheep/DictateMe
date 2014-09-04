c1dfbe008dcd2c50d66b1dd215483ff4e2961bf3
Minseon Song <songminseonk@gmail.com>
2014-09-04 12:07:29 -0400

    github connected

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,15 +1,24 @@
 'use strict';
 
 angular.module('dictateMeApp')
-  .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'FAQ',
-      'link': '/faq'
-    }];
+    .controller('NavbarCtrl', function($scope, $location) {
+        $scope.menu = [{
+            'title': 'How To Use',
+            'link': '',
+            'modalId': '#howTo'
+        }, {
+            'title': 'About',
+            'link': '',
+            'modalId': '#about'
+        }, {
+            'title': 'FAQ',
+            'link': '/faq',
+            'modalId': ''
+        }];
 
-    $scope.isCollapsed = true;
+        $scope.isCollapsed = true;
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+        $scope.isActive = function(route) {
+            return route === $location.path();
+        };
+    });
